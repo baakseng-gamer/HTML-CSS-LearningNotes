6dea594112782457c2e604db5650fb6eb2cea4ce
baakseng-gamer <baakseng.gamer@gmail.com>
2020-08-05 17:42:21 +0800

    StudyNote

---
+++ 2020Web HTML CSS/01-130 表单扩展之数据传输/data.php
@@ -0,0 +1,16 @@
+
+<?php
+
+   /*  $username = $_GET['username'];
+
+    echo $username; */
+
+   /*  $username = $_POST['username'];
+
+    echo $username; */
+
+    $filename = $_FILES['filename'];
+
+    print_r( $filename );
+
+?>
